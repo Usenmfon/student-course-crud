673c4055074772fc43e73342894cdb7c9de25dcc
Usenmfon <usenmfonuko@gmail.com>
2024-01-18 02:31:00 +0100

    update: courses table

--- database/migrations/2024_01_17_103958_create_courses_table.php
+++ database/migrations/2024_01_17_103958_create_courses_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('code');
+            $table->string('name')->unique();
+            $table->string('code')->unique();
             $table->string('department')->nullable();
             $table->string('instructor')->nullable();
             $table->string('semester')->nullable();
